4928b3956191464a6f273e812ad5c1f6feaf9e00
Erwan <55313521+ErwanExplorer@users.noreply.github.com>
2024-07-18 16:26:21 +0200

    Update hero.tsx

--- components/component/hero.tsx
+++ components/component/hero.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-interface IconProps extends React.SVGProps<SVGSVGElement> {}
-
 export function Hero() {
   return (
     <section className="w-full py-16 md:py-24 lg:py-32">
@@ -12,9 +10,8 @@ export function Hero() {
               <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-6xl lg:text-6xl">Erwan Sagnardon</h1>
               <p className="text-muted-foreground text-lg md:text-xl">Software Engineer</p>
             </div>
-           
             <p className="max-w-[600px] text-muted-foreground md:text-xl">
-              Développeur Web passionné d'astronomie et d'aérospatiale. Mon parcours professionnel dans le développement web coexiste harmonieusement avec ma fascination pour les étoiles et l'univers. Mon rêve est de devenir ingénieur spatial, contribuant ainsi à l'exploration de l'infini.
+              Développeur Web passionné d&apos;astronomie et d&apos;aérospatiale. Mon parcours professionnel dans le développement web coexiste harmonieusement avec ma fascination pour les étoiles et l&apos;univers. Mon rêve est de devenir ingénieur spatial, contribuant ainsi à l&apos;exploration de l&apos;infini.
             </p>
           </div>
           <img
@@ -27,106 +24,5 @@ export function Hero() {
         </div>
       </div>
     </section>
-  )
-}
-
-const BoxIcon: React.FC<IconProps> = (props) => {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z" />
-      <path d="m3.3 7 8.7 5 8.7-5" />
-      <path d="M12 22V12" />
-    </svg>
-  )
-}
-
-const CloudIcon: React.FC<IconProps> = (props) => {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" />
-    </svg>
-  )
-}
-
-const CodeIcon: React.FC<IconProps> = (props) => {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <polyline points="16 18 22 12 16 6" />
-      <polyline points="8 6 2 12 8 18" />
-    </svg>
-  )
-}
-
-const DatabaseIcon: React.FC<IconProps> = (props) => {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <ellipse cx="12" cy="5" rx="9" ry="3" />
-      <path d="M3 5V19A9 3 0 0 0 21 19V5" />
-      <path d="M3 12A9 3 0 0 0 21 12" />
-    </svg>
-  )
-}
-
-const XIcon: React.FC<IconProps> = (props) => {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M18 6 6 18" />
-      <path d="m6 6 12 12" />
-    </svg>
   )
 }
