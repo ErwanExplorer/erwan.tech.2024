e6c55bc38b1231477e2d2d57be27e89a21e96cd7
Erwan <55313521+ErwanExplorer@users.noreply.github.com>
2024-07-22 12:43:45 +0200

    Update hero.tsx

--- components/component/hero.tsx
+++ components/component/hero.tsx
@@ -7,11 +7,11 @@ export function Hero() {
         <div className="grid gap-8 md:grid-cols-[1fr_400px] md:gap-12 lg:grid-cols-[1fr_550px]">
           <div className="space-y-4">
             <div className="space-y-2">
-              <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-6xl lg:text-6xl">Erwan Sagnardon</h1>
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-6xl lg:text-6xl">I'm Erwan Sagnardon</h1>
               <p className="text-muted-foreground text-lg md:text-xl">Software Engineer</p>
             </div>
             <p className="max-w-[600px] text-muted-foreground md:text-xl">
-              Développeur Web passionné d'astronomie et d'aérospatiale. Mon parcours professionnel dans le développement web coexiste harmonieusement avec ma fascination pour les étoiles et l'univers. Mon rêve est de devenir ingénieur spatial, contribuant ainsi à l'exploration de l'infini.
+            Je suis un développeur web fullstack basée sur Rouen, France. Je suis passionné d'astronomie et d'aérospatiale. Mon parcours professionnel dans le développement web coexiste harmonieusement avec ma fascination pour les étoiles et l'univers. Mon rêve est de devenir ingénieur spatial, contribuant ainsi à l'exploration de l'infini.
             </p>
           </div>
           <img
